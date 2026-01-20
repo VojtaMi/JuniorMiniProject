cfa4a71c5e2de3bb0642c9b46ecacc4d71c5be76
Mikel Vojtěch <vmikel@trask.cz>
2026-01-20 15:02:22 +0100

    input validation finished (squash)

--- frontend-vanilla/src/main.ts
+++ frontend-vanilla/src/main.ts
@@ -2,25 +2,32 @@ import './styles/main.css';
 import initPhone from './initFields/phone';
 import initEmail from './initFields/email';
 import initGender from './initFields/gender';
-import {initFirstName, initLastName, initCity, initStreet} from './initFields/notEmpty';
+import {initFirstName, initLastName} from './initFields/notEmpty';
+import {initCity, initStreet, initNote} from './initFields/optionalString';
 import initHouseNumber from './initFields/houseNumber';
+import initZipCode from './initFields/zipCode';
+import initBirthDate from './initFields/birthDate';
 import submitContact from './submit'
 
 
 
 // Wait until the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
-  const firstNameAdapter = initFirstName();
-  const lastNameAdapter = initLastName();
-  const emailAdapter = initEmail();
-  const genderAdapter = initGender();
-  const phoneAdapter = initPhone();
-  const cityAdapter = initCity();
-  const streetAdapter = initStreet();
+  const firstName = initFirstName();
+  const lastName = initLastName();
+  const email = initEmail();
+  const gender = initGender();
+  const birthDate = initBirthDate();
+  const phone = initPhone();
+  const city = initCity();
+  const street = initStreet();
   const houseNumber = initHouseNumber();
+  const zipCode = initZipCode();
+  const note = initNote();
+  
 
   // const apiEndpoint = 'http://localhost:3333/api/contacts',
-  submitContact(firstNameAdapter, lastNameAdapter,emailAdapter, genderAdapter, phoneAdapter);
+  submitContact(firstName, lastName, email, gender, birthDate, phone, city, street, houseNumber, zipCode, note);
 });
 
 
@@ -34,8 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //
 // API endpointy jsou dostupné na: http://localhost:3333/api/contacts
 // Dokumentace API: http://localhost:3333/swagger
-
-console.log('Vanilla TypeScript frontend připraven k implementaci!');
 
 
 const app = document.getElementById('app');
