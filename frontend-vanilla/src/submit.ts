0eb2f86204fdb8170a9131421f9fdc7bf5fe10ab
Mikel Vojtěch <vmikel@trask.cz>
2026-01-20 16:27:03 +0100

    object for inputs

--- frontend-vanilla/src/submit.ts
+++ frontend-vanilla/src/submit.ts
@@ -1,16 +1,5 @@
 
-export default function submitContact(
-    firstName: () => string,
-    lastName: () => string,
-    email: () => string,
-    gender: () => string,
-    birthDate: () => string,
-    phone: () => string,
-    city: () => string,
-    street: () => string,
-    houseNumber: () => string,
-    zipCode: () => string,
-    note: () => string) {
+export default function submitContact(inputs: Record<string, ()=>string>) {
     const form = document.querySelector('#contact-form') as HTMLFormElement | null;
 
     if (!form) {
@@ -19,16 +8,8 @@ export default function submitContact(
     }
     form.addEventListener('submit', event => {
         event.preventDefault();
-        console.log("first name: " + firstName());
-        console.log("last name: " + lastName());
-        console.log("email: " + email());
-        console.log("gender: " + gender());
-        console.log("birth date: " + birthDate());
-        console.log("phone: " + phone());
-        console.log("city: " + city());
-        console.log("street: " + street());
-        console.log("house number: " + houseNumber());
-        console.log("ZIP code: " + zipCode());
-        console.log("note: " + note());
+        for (const input in inputs){
+            console.log(`${input}: ${inputs[input]()}`);
+        }
     })
 }
