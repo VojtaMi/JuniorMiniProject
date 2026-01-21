4f1238be929ae3dd85543d1e64affb1b97eb543e
Mikel Vojtěch <vmikel@trask.cz>
2026-01-21 11:26:44 +0100

    w3 buttons reacting

--- frontend-vanilla/src/main.ts
+++ frontend-vanilla/src/main.ts
@@ -7,6 +7,20 @@ import handleSubmit from './submit'
 document.addEventListener('DOMContentLoaded', () => {
   // const apiEndpoint = 'http://localhost:3333/api/contacts',
   handleSubmit(inputs);
+  const newContactBtn = document.getElementById("new-contact-btn");
+  if (!newContactBtn){
+    console.log("no newContactBtn")
+  }else{
+    newContactBtn.addEventListener("click", () => console.log("newContactBtn pressed"));
+  }
+
+  const contactListBtn = document.getElementById("contact-list-btn");
+  if (!contactListBtn) {
+    console.log("no contactListBtn")
+  } else {
+    contactListBtn.addEventListener("click", () => console.log("contactListBtn pressed"));
+  }
+  
 });
 
 
@@ -20,11 +34,4 @@ document.addEventListener('DOMContentLoaded', () => {
 //
 // API endpointy jsou dostupné na: http://localhost:3333/api/contacts
 // Dokumentace API: http://localhost:3333/swagger
-
-
-const app = document.getElementById('app');
-
-if (app) {
-
-}
 
