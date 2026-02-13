8890273a02628a0de6a42de727d5116dc19332e7
Mikel Vojtěch <vmikel@trask.cz>
2026-02-13 09:26:39 +0100

    crateContact in fetching hook

---
+++ frontend/src/hooks/fetching/useCreateContact.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { contactsApi } from "../../api/contactsApi";
+import type { Contact } from "../../types/contact";
+
+interface UseCreateContactsResult {
+  fetchedContact: Contact;
+  error: string | null;
+  isFetching: boolean;
+}
+
+export function useCreateContacts(
+  contactData: Contact
+): UseCreateContactsResult {
+  const [fetchedContact, setFetchedContact] = useState<Contact>({firstName:'', lastName:'', email:''});
+  const [error, setError] = useState<string | null>(null);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  useEffect(() => {
+    let isActive = true;
+
+    const loadContacts = async () => {
+      try {
+        setIsFetching(true);
+        const data = await contactsApi.createContact(contactData);
+        if (isActive) {
+          setFetchedContact(data);
+        }
+      } catch {
+        if (isActive) {
+          setError("Nepodařilo se načíst data.");
+        }
+      } finally {
+        if (isActive) {
+          setIsFetching(false);
+        }
+      }
+    };
+
+    loadContacts();
+
+    return () => {
+      isActive = false;
+    };
+  }, [contactData]);
+
+  return { fetchedContact, error, isFetching };
+}
